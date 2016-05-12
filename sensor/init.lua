2b9119cd19b10d04951e34f0984a3146b45e7cb3
Elliot Williams <elliot@pre.postero.us>
2016-05-12 19:59:22 +0200

    added verbose printing to WiFi connection
    helps debug bad PW / AP name

--- sensor/init.lua
+++ sensor/init.lua
@@ -1,28 +1,48 @@
-WIFI_ESSID=""
-WIFI_PASSWORD=""
-EXEC_FILE="mqtt_temp_node.lua"
-
+WIFI_ESSID    = ""
+WIFI_PASSWORD = ""
+EXEC_FILE     = "mqtt_temp_node.lua"
+timeout       = 15
 
 wifi.setmode(wifi.STATION)
 wifi.sta.config(WIFI_ESSID, WIFI_PASSWORD)
-counts = 1
 
--- Check for 
-tmr.alarm(0, 1*1000, tmr.ALARM_AUTO, 
-	function() 
-	if wifi.sta.getip()==nil then	
+function print_wifi_status(status)
+	if status == wifi.STA_WRONGPWD then
+		print("wrong passsword")
+	elseif status == wifi.STA_APNOTFOUND then
+		print("can't find AP")
+	elseif status == wifi.STA_FAIL then
+		print("failed")
+	elseif status == wifi.STA_GOTIP then 
+		print("got IP")
+	end
+end
+
+-- Loop until WiFi configured, then branch
+-- If there's a configuration problem with AP name or PW, 
+--   delete the init.lua file and reboot.
+counts  = 1
+tmr.alarm(0, 1*1000, tmr.ALARM_AUTO, function() 
+	status = wifi.sta.status() 
+	print_wifi_status(status)
+	if (status == wifi.STA_WRONGPWD or status == wifi.STA_APNOTFOUND) then
+		print("wifi incorrectly configured: removing init.lua")
+		file.remove("init.lua")
+		tmr.unregister(0)
+		node.restart()
+	end
+	if status ~= wifi.STA_GOTIP then	
 		print("waiting for wifi " .. counts)
 		counts = counts + 1
-		if counts > 25 then  -- timeout
+		if counts > timeout then  -- timeout
 			tmr.unregister(0)
-			print("wifi timeout: removing init.lua and restarting...")
-			file.remove("init.lua")
+			print("failed to acquire IP address, restarting")
 			node.restart()
 		end
-						
+
 	else
 		tmr.unregister(0)
-		print('ip address', wifi.sta.getip()) 
+		print(wifi.sta.getip()) 
 		dofile(EXEC_FILE)
 	end 
 end
@@ -30,4 +50,5 @@ end
 
 -- cancel init just in case it's porked
 -- file.remove("init.lua")
+
 
